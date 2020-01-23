f8ea39ff54bf8b7efd9b52bf6e18738be23f803d
Christian Arzate <carzate.fr@gmail.com>
2020-01-23 11:52:13 -0600

    Portafolio en Firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { infoPaginaService } from './services/info-pagina.service';
+import { ProductosService } from './services/productos.service';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,10 @@ import { infoPaginaService } from './services/info-pagina.service';
 })
 export class AppComponent {
 
-  constructor( public infoPaginaService: infoPaginaService) {
+  constructor( public infoPaginaService: infoPaginaService,
+               public ProductosService: ProductosService ) {
 
   }
+
+
 }
--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -12,20 +12,37 @@ export class infoPaginaService {
   info: infoPagina = {};
   cargada: boolean= false;
 
+  equipo: any[] = [];
+
 
 
   constructor( private http: HttpClient){
 
     // console.log('Servicio de infopagina listo');
 
+    this.cargarInfo();
+    this.cargarEquipo();
+
+  }
+
+  private cargarInfo() {
+
     //Leer el archivo JSON
     this.http.get('assets/data/data-pages.json').subscribe( (resp: infoPagina) => {
 
-        this.cargada = true;
-        this.info = resp;
-        console.log(resp);
+      this.cargada = true;
+      this.info = resp;
 
-      });
+    });
+  }
 
+  private cargarEquipo(){
+
+   //Leer el archivo JSON
+   this.http.get('https://angular-portafolio-69137.firebaseio.com/equipo.json').subscribe( (resp: any ) => {
+
+    this.equipo = resp;
+
+  });
   }
 }
